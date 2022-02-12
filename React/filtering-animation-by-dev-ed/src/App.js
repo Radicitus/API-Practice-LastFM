783e295c2cb65defaef442f0898c82299606441f
Radicitus <novivolatus@gmail.com>
2022-02-11 19:06:33 -0800

    Return Movie component

---
+++ React/filtering-animation-by-dev-ed/src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import Movie from "./Movie";
+
+function App() {
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+
+  const fetchPopular = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=6af6b9ee971fa088b1204e3f9743c8ad"
+    );
+    const movies = await data.json();
+    console.log(movies);
+    setPopular(movies.results);
+  };
+
+  return (
+    <div className="App">
+      <div className="popular-movies">
+        {popular.map((movie) => {
+          return <Movie />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
